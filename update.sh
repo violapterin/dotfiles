e0a00181ae544132c5fab708fadd0578aa91f5ac
aminopterin <archinewtogauss@gmail.com>
2017-01-16 18:43:57 +0800

    Added .bash_profile and .bashrc

--- update.sh
+++ update.sh
@@ -1,6 +1,29 @@
 #! /usr/bin/env bash
+# To update newest version of templates and scripts.
 
-cp -f ~/.vimrc ./vimrc
-cp -f ~/.bash_profile ./.bash_profile
-cp -f ~/template-CJK.tex ./template-CJK.tex
-cp -f ~/template-Latin.tex ./template-Latin.tex
+NUM_DOC=11 # number of documents
+COMMAND="cp -f" # command to be applied
+
+list_files=(
+   .vimrc
+   .bash_profile 
+   .bashrc
+   template_pandoc.tex
+   template_Latin.tex
+   template_CJK.tex
+   run_pandoc.sh
+   run_pdflatex.sh
+   run_xelatex.sh
+   makefile_pandoc
+   makefile_pdflatex
+   makefile_xelatex
+)
+
+dir_from="~"
+dir_to="dirname $0"
+
+
+for (( i=1; i<=NUM_DOC; i++ ));
+do
+   ${CMD} ${dir_from}\/${list_files[i]} ${dir_to}\/${list_files[i]}
+done
