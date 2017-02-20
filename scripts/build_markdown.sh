ec47db22f03c2a054cc51edaf87768d17303b207
aminopterin <d.corydaline@gmail.com>
2017-02-20 23:03:28 +0800

    Complete tex builder by imitating GNU Make, untested

--- scripts/build_markdown.sh
+++ scripts/build_markdown.sh
@@ -28,14 +28,14 @@ EXT_BIN=".pdf"
 # or in a subfolder, or in a subsubfolder.
 list_src=$(find . -name "*${EXT_SRC}")
 
-# Partial derivative, indicating fragment work.
+# Partial derivative, indicating fragmentary work.
 FRAGMENT="∂"
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 for dir_full_name_src in ${list_src}
 do
-   # Basename of source. If `src` is `foo.bar.baz`, `jobname` is `foo`.
+   # If `src` is `foo.bar.baz`, `bare_name_common` is `foo`.
    full_name_src="$(basename ${dir_full_name_src})"
    dir="$(dirname ${dir_full_name_src})"
    bare_name_common="${full_name_src%%.*}"
@@ -47,10 +47,6 @@ do
       continue
    fi
 
-#echo "full_name_src=${full_name_src}"
-#echo "bare_name_common=${bare_name_common}"
-#echo "full_name_bin=${full_name_bin}"
-
    # Check timestamp according to dependency.
    whether_make=$(resp_old_new "${dir_full_name_src}" "${dir}/${full_name_bin}")
    if [ "${whether_make}" == "TRUE" ]; then
@@ -60,5 +56,6 @@ do
    # Compilation of the binary file.
    echo "Compiling ${full_name_bin} from ${full_name_src} ..."
    "${FULL_PRG}" "${dir_full_name_src}" "${bare_name_common}"
+
    mv "${full_name_bin}" "${dir}/${full_name_bin}"
 done
