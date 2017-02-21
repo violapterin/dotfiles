8057d9e1a885b4b6ab3bffda95c1b3ec9b10009c
aminopterin <d.corydaline@gmail.com>
2017-02-21 20:58:28 +0800

    Fix share_tex_family.sh; untested

--- scripts/build_markdown.sh
+++ scripts/build_markdown.sh
@@ -24,23 +24,25 @@ RM="rm -f"
 EXT_SRC=".md"
 EXT_BIN=".pdf"
 
-# To set the Markdown source files in the top directory,
-# or in a subfolder, or in a subsubfolder.
-list_src=$(find . -name "*${EXT_SRC}")
+# To set the Markdown sources inside current working directory.
+list_global_src=$(find . -name "*${EXT_SRC}")
 
 # Partial derivative, indicating fragmentary work.
 FRAGMENT="∂"
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-for dir_full_name_src in ${list_src}
+for dir_full_name_src in ${list_global_src}
 do
    # If `src` is `foo.bar.baz`, `bare_name_common` is `foo`.
-   full_name_src="$(basename ${dir_full_name_src})"
    dir="$(dirname ${dir_full_name_src})"
+   cd "${dir}"
+   full_name_src="$(basename ${dir_full_name_src})"
    bare_name_common="${full_name_src%%.*}"
-   # Name the corresponding binary the same as directory.
+
+   # Name the corresponding binary the same as according source.
    full_name_bin="${bare_name_common}${EXT_BIN}"
+   dir_full_name_bin="${dir}/${full_name_bin}"
 
    # Ignore `∂`-prefixed (unfinished) files.
    if [ "${full_name_src:0:1}" == "${FRAGMENT}" ]; then
@@ -48,14 +50,12 @@ do
    fi
 
    # Check timestamp according to dependency.
-   whether_make=$(resp_old_new "${dir_full_name_src}" "${dir}/${full_name_bin}")
-   if [ "${whether_make}" == "TRUE" ]; then
+   whether_make="$(resp_old_new "${dir_full_name_bin}" "${dir_full_name_src}")"
+   if [ "${whether_make}" == "FALSE" ]; then
       continue
    fi
 
    # Compilation of the binary file.
    echo "Compiling ${full_name_bin} from ${full_name_src} ..."
    "${FULL_PRG}" "${dir_full_name_src}" "${bare_name_common}"
-
-   mv "${full_name_bin}" "${dir}/${full_name_bin}"
 done
--- scripts/share_tex_family.sh
+++ scripts/share_tex_family.sh
@@ -21,27 +21,35 @@ source "${DIR_SCRIPTS}/${NAME_FUN_DEF}"
 # To delete binary files.
 RM="rm -f"
 
-# extensions resp. of souce and binary files
+# Defined names of extensions resp. of souce and binary files.
+# Compiler reads `main.tex`, & join sources as `standalone.tex`
 EXT_SRC=".tex"
 EXT_BIN=".pdf"
+BARE_NAME_SRC_CHIEF="main"
+BARE_NAME_SRC_ALL="standalone"
+FULL_NAME_SRC=${BARE_NAME_SRC_CHIEF}${EXT_SRC}
 
-# To set the LaTeX source files in the top directory,
-# or in a subfolder, or in a subsubfolder.
-list_src=$(find . -name "*${EXT_SRC}")
+# To set the `main.tex` sources inside current working directory.
+list_chief_src=$(find . -name "${FULL_NAME_SRC}")
 
 # Partial derivative, indicating fragmentary work.
 FRAGMENT="∂"
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-for dir_full_name_src in ${list_src}
+for dir_full_name_chief_src in ${list_chief_src}
 do
-   # If `src` is `foo.bar.baz`, `bare_name_common` is `foo`.
-   full_name_src="$(basename ${dir_full_name_src})"
    dir="$(dirname ${dir_full_name_src})"
-   bare_name_common="${full_name_src%%.*}"
+   cd "${dir}"
+   # To hold all sources in the current working directory.
+   list_relavent_src=$(find . -name "*${EXT_SRC}")
+   # To remove the `standalone.tex` generated from `main.tex`.
+   list_relavent_src="${list_relavent_src/${FULL_NAME_SRC}/}"
+
    # Name the corresponding binary the same as directory.
-   full_name_bin="${bare_name_common}${EXT_BIN}"
+   bare_name_bin="$(basename ${dir})"
+   full_name_bin="${bare_name_bin}${EXT_BIN}"
+   dir_full_name_bin="${dir}/${full_name_bin}"
 
    # Ignore `∂`-prefixed (unfinished) files.
    if [ "${full_name_src:0:1}" == "${FRAGMENT}" ]; then
@@ -49,15 +57,20 @@ do
    fi
 
    # Check timestamp according to dependency.
-   whether_make=$(resp_old_new "${dir_full_name_src}" "${dir}/${full_name_bin}")
-   if [ "${whether_make}" == "TRUE" ]; then
+   "${whether_make}" == "FALSE"
+   for full_name_src in "${list_relavent_src}"
+   do
+      hold="$(resp_old_new "${dir_full_name_bin}" "${dir_full_name_src}")"
+      if [ "${hold}" == "TRUE" ]; then
+         whether_make="${hold}"
+         break
+      fi
+   done
+   if [ "${whether_make}" == "FALSE" ]; then
       continue
    fi
 
    # Compilation of the binary file.
-   echo "Compiling ${full_name_bin} from ${full_name_src} ..."
-   "${FULL_PRG}" "${dir_full_name_src}" "${bare_name_common}"
-
-   last_dir="$(basename ${dir})"
-   mv "${full_name_bin}" "${dir}/${last_dir}${EXT_BIN}"
+   echo "Compiling ${full_name_bin} from ${FULL_NAME_SRC} ..."
+   "${FULL_PRG}" "${dir_full_name_src}" "${bare_name_bin}"
 done
