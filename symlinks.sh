90a651ddaef31948e91b269f91d38d1c2cd4b46b
Raven I. Pilot <violapterin@gmail.com>
2020-07-15 17:51:05 +0800

    Delete ssh line, vim line

--- symlinks.sh
+++ symlinks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#here="${REPOS}/code/dotfiles"
-here="/home/violapterin/Dropbox/repositories/code/dotfiles"
+#here="/home/violapterin/Dropbox/repositories/code/dotfiles"
+here="$(dirname "$(readlink -f "$0")")"
 
 # system
 ln -sf "${here}/.profile" "${HOME}/.profile"
@@ -9,17 +9,11 @@ ln -sf "${here}/.bash_profile" "${HOME}/.bash_profile"
 ln -sf "${here}/.bashrc" "${HOME}/.bashrc"
 ln -sf "${here}/.xinitrc" "${HOME}/.xinitrc"
 
-# Vim, NeoVim
-ln -sf "${here}/colors" "${HOME}/.vim/colors"
-ln -sf "${here}/.vimrc" "${HOME}/.vimrc"
+# NeoVim
 ln -sf "${here}/colors" "${HOME}/.config/nvim/colors"
 ln -sf "${here}/.vimrc" "${HOME}/.config/nvim/init.vim"
 
 # others configurations
 ln -sf "${here}/.gitconfig" "${HOME}/.gitconfig"
 ln -sf "${here}/.tmux.conf" "${HOME}/.tmux.conf"
-
-# private keys
-ln -sf "${here}/.ssh" "${HOME}/.ssh"
-ln -sf "${here}/.gnupg" "${HOME}/.gnupg"
 
